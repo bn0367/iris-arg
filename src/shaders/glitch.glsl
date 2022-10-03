753fa78e80b5e9589dfc8f1bfdde7c4901467eee
bn0367 <bnewman@oberlin.edu>
2022-10-02 23:55:07 -0400

    shaders p3

--- src/shaders/glitch.glsl
+++ src/shaders/glitch.glsl
@@ -9,33 +9,24 @@ vec2 sat(vec2 t) {
 }
 
 //remaps inteval [a;b] to [0;1]
-float remap  (float t, float a, float b) {
+float remap(float t, float a, float b) {
     return sat((t - a) / (b - a));
 }
 
 //note: /\ t=[0;0.5;1], y=[0;1;0]
 float linterp(float t) {
-    return sat(1.0 - abs(2.0*t - 1.0));
+    return sat(1.0 - abs(2.0 * t - 1.0));
 }
 
 vec3 spectrum_offset(float t) {
     float t0 = 3.0 * t - 1.5;
     //return vec3(1.0/3.0);
-    return clamp(vec3(-t0, 1.0-abs(t0), t0), 0.0, 1.0);
-    /*
-	vec3 ret;
-	float lo = step(t,0.5);
-	float hi = 1.0-lo;
-	float w = linterp( remap( t, 1.0/6.0, 5.0/6.0 ) );
-	float neg_w = 1.0-w;
-	ret = vec3(lo,1.0,hi) * vec3(neg_w, w, neg_w);
-	return pow( ret, vec3(1.0/2.2) );
-*/
+    return clamp(vec3(-t0, 1.0 - abs(t0), t0), 0.0, 1.0);
 }
 
 //note: [0;1]
 float rand(vec2 n) {
-    return fract(sin(dot(n.xy, vec2(12.9898, 78.233)))* 43758.5453);
+    return fract(sin(dot(n.xy, vec2(12.9898, 78.233))) * 43758.5453);
 }
 
 //note: [-1;1]
@@ -45,11 +36,11 @@ float srand(vec2 n) {
 
 float mytrunc(float x, float num_levels)
 {
-    return floor(x*num_levels) / num_levels;
+    return floor(x * num_levels) / num_levels;
 }
 vec2 mytrunc(vec2 x, float num_levels)
 {
-    return floor(x*num_levels) / num_levels;
+    return floor(x * num_levels) / num_levels;
 }
 
 uniform float time;
@@ -63,23 +54,23 @@ void main()
 
     float time2 = mod(time, 32.0);// + modelmat[0].x + modelmat[0].z;
 
-    float GLITCH = 0.1;
+    float GLITCH = rand(vec2(time2 * 0.1, 0.0)) * 0.5 + 0.5;
 
     //float rdist = length( (uv - vec2(0.5,0.5))*vec2(aspect, 1.0) )/1.4;
     //GLITCH *= rdist;
 
     float gnm = sat(GLITCH);
     float rnd0 = rand(mytrunc(vec2(time2, time2), 6.0));
-    float r0 = sat((1.0-gnm)*0.7 + rnd0);
-    float rnd1 = rand(vec2(mytrunc(uv.x, 10.0*r0), time2));//horz
+    float r0 = sat((1.0 - gnm) * 0.7 + rnd0);
+    float rnd1 = rand(vec2(mytrunc(uv.x, 10.0 * r0), time2));//horz
     //float r1 = 1.0f - sat( (1.0f-gnm)*0.5f + rnd1 );
     float r1 = 0.5 - 0.5 * gnm + rnd1;
-    r1 = 1.0 - max(0.0, ((r1<1.0) ? r1 : 0.9999999));//note: weird ass bug on old drivers
-    float rnd2 = rand(vec2(mytrunc(uv.y, 40.0*r1), time2));//vert
+    r1 = 1.0 - max(0.0, ((r1 < 1.0) ? r1 : 0.9999999));//note: weird ass bug on old drivers
+    float rnd2 = rand(vec2(mytrunc(uv.y, 40.0 * r1), time2));//vert
     float r2 = sat(rnd2);
 
-    float rnd3 = rand(vec2(mytrunc(uv.y, 10.0*r0), time2));
-    float r3 = (1.0-sat(rnd3+0.8)) - 0.1;
+    float rnd3 = rand(vec2(mytrunc(uv.y, 10.0 * r0), time2));
+    float r3 = (1.0 - sat(rnd3 + 0.8)) - 0.1;
 
     float pxrnd = rand(uv + time2);
 
@@ -93,7 +84,7 @@ void main()
 
     vec4 sum = vec4(0.0);
     vec3 wsum = vec3(0.0);
-    for (int i=0; i<NUM_SAMPLES; ++i)
+    for (int i = 0; i < NUM_SAMPLES; ++i)
     {
         float t = float(i) * RCP_NUM_SAMPLES_F;
         uv.x = sat(uv.x + ofs * t);
